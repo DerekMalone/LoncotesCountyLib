1be83414760e390c59337c22f2d27791eba0933a
Derek Malone <laxmalone@gmail.com>
2025-04-22 21:41:47 -1000

    initial project set up and original commit

---
+++ Models/Patron.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LoncotesCountyLib.Models;
+
+public class Patron
+{
+    public int Id {get; set;}
+    [Required]
+    public string FirstName { get; set;}
+    [Required]
+    public string LastName {get; set;}
+    public string Address {get; set;}
+    public string Email {get; set;}
+    public bool IsActive {get; set;} 
+}
