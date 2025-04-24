6b452e10fd8635708f20b4e5e8acd4fccce12680
Derek Malone <laxmalone@gmail.com>
2025-04-23 21:01:35 -1000

    completed file and data setup. Pushing to initialize data migrations

--- LoncotesCountyLibDbContext.cs
+++ LoncotesCountyLibDbContext.cs
@@ -26,17 +26,32 @@ public class LoncotesCountyLibDbContext : DbContext
         modelBuilder.Entity<MaterialType>().HasData(new MaterialType[]
         {
             new MaterialType {Id = 1, Name = "Book", CheckoutDays = 7},
-            // ! Need to add  2 more MaterialType
+            new MaterialType {Id = 2, Name = "Newspaper", CheckoutDays = 3},
+            new MaterialType {Id = 3, Name = "CD", CheckoutDays = 5},
+            new MaterialType {Id = 4, Name = "8Track", CheckoutDays = 5}
         });
 
         modelBuilder.Entity<Genre>().HasData(new Genre[]
         {
-            // ! Need to add 5 Genres
+            new Genre {Id = 1, Name = "Scy-Fi"},
+            new Genre {Id = 2, Name = "Biography"},
+            new Genre {Id = 3, Name = "Fantasy"},
+            new Genre {Id = 4, Name = "Thriller"},
+            new Genre {Id = 5, Name = "Cooking"}
         });
 
         modelBuilder.Entity<Material>().HasData(new Material[]
         {
-            // ! Need to add 10 Materials
+            new Material {Id = 1, MaterialName = "Lord of the Rings", MaterialTypeId = 1, GenreId = 3, OutOfCirculationSince = new DateTime(2016, 05, 01)},
+            new Material {Id = 2, MaterialName = "The NY Times", MaterialTypeId = 2, GenreId = 5, OutOfCirculationSince = new DateTime(2020, 07, 12)},
+            new Material {Id = 3, MaterialName = "Abraham Lincoln, Vampire Slayer", MaterialTypeId = 1, GenreId = 2, OutOfCirculationSince = new DateTime(2000, 01, 29)},
+            new Material {Id = 4, MaterialName = "Sherlock Holmes", MaterialTypeId = 2, GenreId = 4, OutOfCirculationSince = new DateTime(2015, 09, 01)},
+            new Material {Id = 5, MaterialName = "Bethovens 5th", MaterialTypeId = 3, GenreId = 4, OutOfCirculationSince = new DateTime(1989, 06, 16)},
+            new Material {Id = 6, MaterialName = "Isaac Hayes", MaterialTypeId = 4, GenreId = 1, OutOfCirculationSince = new DateTime(1997, 05, 05)},
+            new Material {Id = 7, MaterialName = "The Expanse", MaterialTypeId = 1, GenreId = 1, OutOfCirculationSince = new DateTime(2015, 04, 02)},
+            new Material {Id = 8, MaterialName = "Dungeon Crawler Carl", MaterialTypeId = 1, GenreId = 3, OutOfCirculationSince = new DateTime(2017, 11, 11)},
+            new Material {Id = 9, MaterialName = "Good Charlotte", MaterialTypeId = 3, GenreId = 3, OutOfCirculationSince = new DateTime(2001, 12, 03)},
+            new Material {Id = 10, MaterialName = "Paleo Diet", MaterialTypeId = 4, GenreId = 5, OutOfCirculationSince = new DateTime(2016, 08, 19)}
         });
         
     }
