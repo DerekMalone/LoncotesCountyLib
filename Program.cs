6321362bffd74166f0511fdf2b5120fce261272e
Derek Malone <laxmalone@gmail.com>
2025-04-24 22:09:48 -1000

    completed remove material from circulation soft delete

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/materials", (int? materialTypeId, int? genreId, LoncotesCountyLibDbContext db) => 
+app.MapGet("/api/materials", (LoncotesCountyLibDbContext db, int? materialTypeId, int? genreId) => 
 {
     // ! Need to logic through using query params established above. 
     // ! Could do if statements? Commented out code at bottom works but I didn't write it...
@@ -75,7 +75,36 @@ app.MapGet("/api/materials", (int? materialTypeId, int? genreId, LoncotesCountyL
     return Results.Ok(materials);
 });
 
+app.MapGet("/api/materials/{id}", (LoncotesCountyLibDbContext db, int id) => 
+{
+    var material = db.Materials.Where(m => m.Id == id)
+    .Select(m => new MaterialDTO
+    {
+        Id = m.Id,
+        MaterialName = m.MaterialName,
+        MaterialTypeId = m.MaterialTypeId,
+        MaterialType = new MaterialTypeDTO
+        {
+            Id = m.MaterialType.Id,
+            Name = m.MaterialType.Name,
+            CheckoutDays = m.MaterialType.CheckoutDays
+        },
+        GenreId = m.GenreId,
+        Genre = new GenreDTO
+        {
+            Id = m.Genre.Id,
+            Name = m.Genre.Name
+        },
+        OutOfCirculationSince = m.OutOfCirculationSince
+    }).SingleOrDefault();
 
+    if (material != null)
+    {
+    return Results.Ok(material);
+    }
+
+    return Results.NotFound();
+});
 
 app.MapPost("/api/materials", (LoncotesCountyLibDbContext db, Material material) =>
 {
@@ -84,9 +113,24 @@ app.MapPost("/api/materials", (LoncotesCountyLibDbContext db, Material material)
     return Results.Created($"/api/materials/{material.Id}", material);
 });
 
+app.MapPut("/api/materials/{id}/delete", (LoncotesCountyLibDbContext db, int id) => 
+{
+    Material materialToUpdate = db.Materials.SingleOrDefault(m => m.Id == id);
+    
+    if (materialToUpdate == null)
+    {
+    return Results.NotFound();
+    }
+
+    materialToUpdate.OutOfCirculationSince = DateTime.Now;
+
+    db.SaveChanges();
+    return Results.NoContent();
+    
+});
+
 app.Run();
 
-// ! Current status, Create get single Material
 
-
+// ! Current status, Get Material Types
 // ? https://github.com/nashville-software-school/server-side-dotnet-curriculum/blob/main/book-3-sql-efcore/chapters/loncotes-basic-features.md
