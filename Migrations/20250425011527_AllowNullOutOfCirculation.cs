e08081342a08f7dc70fc5aba3f58e01a9fc99a65
Derek Malone <laxmalone@gmail.com>
2025-04-24 16:03:53 -1000

    working through chpt. added initial get materials and add materials but need to filter materials based on query strings input if input...

---
+++ Migrations/20250425011527_AllowNullOutOfCirculation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoncotesCountyLib.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNullOutOfCirculation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OutOfCirculationSince",
+                table: "Materials",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materials_GenreId",
+                table: "Materials",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materials_MaterialTypeId",
+                table: "Materials",
+                column: "MaterialTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Materials_Genres_GenreId",
+                table: "Materials",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Materials_MaterialTypes_MaterialTypeId",
+                table: "Materials",
+                column: "MaterialTypeId",
+                principalTable: "MaterialTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Materials_Genres_GenreId",
+                table: "Materials");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Materials_MaterialTypes_MaterialTypeId",
+                table: "Materials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Materials_GenreId",
+                table: "Materials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Materials_MaterialTypeId",
+                table: "Materials");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OutOfCirculationSince",
+                table: "Materials",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using LoncotesCountyLib.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
+using LoncotesCountyLib.Models.DTO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,10 +29,85 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/api/materials", (int? materialTypeId, int? genreId, LoncotesCountyLibDbContext db) => 
+{
+    // ! Need to logic through using query params established above. 
+    // ! Could do if statements? Commented out code at bottom works but I didn't write it...
+    return Results.Ok(db.Materials
+    .Include(m => m.MaterialType)
+    .Include(m => m.Genre)
+    .Where(g => g.OutOfCirculationSince == null)
+    .Select(m => new MaterialDTO
+    {
+        Id = m.Id,
+        MaterialName = m.MaterialName,
+        MaterialTypeId = m.MaterialTypeId,
+        MaterialType = new MaterialTypeDTO
+        {
+            Id = m.MaterialType.Id,
+            Name = m.MaterialType.Name,
+            CheckoutDays = m.MaterialType.CheckoutDays
+        },
+        GenreId = m.GenreId,
+        Genre = new GenreDTO
+        {
+            Id = m.Genre.Id,
+            Name = m.Genre.Name
+        },
+        OutOfCirculationSince = m.OutOfCirculationSince
+    }).ToList());
+
+    //     IQueryable<Material> query = db.Materials
+    //     .Include(m => m.MaterialType)
+    //     .Include(m => m.Genre)
+    //     .Where(m => m.OutOfCirculationSince == null);
+    
+    // // Apply filters only if parameters are provided
+    // if (materialTypeId != null)
+    // {
+    //     query = query.Where(m => m.MaterialTypeId == materialTypeId);
+    // }
+    
+    // if (genreId != null)
+    // {
+    //     query = query.Where(m => m.GenreId == genreId);
+    // }
+    
+    // List<MaterialDTO> materials = query.Select(m => new MaterialDTO
+    // {
+    //     Id = m.Id,
+    //     MaterialName = m.MaterialName,
+    //     MaterialTypeId = m.MaterialTypeId,
+    //     MaterialType = new MaterialTypeDTO
+    //     {
+    //         Id = m.MaterialType.Id,
+    //         Name = m.MaterialType.Name,
+    //         CheckoutDays = m.MaterialType.CheckoutDays
+    //     },
+    //     GenreId = m.GenreId,
+    //     Genre = new GenreDTO
+    //     {
+    //         Id = m.Genre.Id,
+    //         Name = m.Genre.Name
+    //     },
+    //     OutOfCirculationSince = m.OutOfCirculationSince
+    // }).ToList();
+    
+    // return Results.Ok(materials);
+});
+
+
+
+app.MapPost("/api/materials", (LoncotesCountyLibDbContext db, Material material) =>
+{
+    db.Materials.Add(material);
+    db.SaveChanges();
+    return Results.Created($"/api/materials/{material.Id}", material);
+});
 
 app.Run();
 
-// ! Current status, Working on step 5 of chpt.
-// ! Need to complete LoncotesCountyLibdbContext class model creation with seed data 
+// ! Current status, Working on step 1 of chpt 2
+// ? Just realized that I seeded the db with materials that ALL have out of circulation dates...
 
-// ? https://github.com/nashville-software-school/server-side-dotnet-curriculum/blob/main/book-3-sql-efcore/chapters/loncotes-setup.md
+// ? https://github.com/nashville-software-school/server-side-dotnet-curriculum/blob/main/book-3-sql-efcore/chapters/loncotes-basic-features.md
