af4b114cfba826bb513411f2051898c4a707f4b6
Derek Malone <laxmalone@gmail.com>
2025-05-09 15:46:09 -1000

    setting up late fee calculation

--- Models/DTOs/CheckoutDTO.cs
+++ Models/DTOs/CheckoutDTO.cs
@@ -9,5 +9,19 @@ public class CheckoutDTO
     public PatronDTO Patron {get; set;}
     public DateTime CheckoutDate {get; set;}
     public DateTime? ReturnDate {get; set;}
-    
+    private static decimal _lateFeePerDay = .50M;
+    public decimal? LateFee {get; set;}
+    // below is begining of logic to calculate latefee.
+    // {
+    //     get
+    //     {
+    //         if (daysLate > 0)
+    //         {
+    //             return 0;
+    //             //do logic to return fee...
+    //         }
+    //     return null;
+    //     } 
+    //     set;    
+    // }
 }
